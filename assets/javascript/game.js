592885dae0ac98e36a4467484812d18b73012dea
ava krishn <avakrishn@gmail.com>
2018-06-10 00:04:15 -0700

    added pictures for easy word list that is displayed when the game is won

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,11 @@
 
 // Word Lists (default = easyList)
 var easyList =['DENMARK', 'FINLAND', 'FRANCE', 'GERMANY', 'GREECE', 'HUNGARY', 'IRELAND', 'ITALY', 'SPAIN', 'GREENLAND', 'SWITZERLAND', 'SWEDEN', 'SCOTLAND', 'UNITED KINGDOM'];
-// var easyImage =['assets/images/flags/denmark.png', 'assets/images/flags/finland.png'];
-// ['ITALY', 'IRELAND','UNITED KINGDOM', 'SPAIN', 'PORTUGAL', 'GERMANY', 'AUSTRIA', 'BELGIUM', 'CZECH REPUBLIC', 'FRANCE'];
 var medList =['AUSTRIA', 'BELGIUM', 'BULGARIA' , 'CROATIA' ,'CZECH REPUBLIC', 'NETHERLANDS', 'NORWAY', 'POLAND', 'PORTUGAL','ROMANIA']; 
 var hardList =['ALBANIA', 'BELARUS', 'BASQUE', 'CATALONIA', 'CYPRUS', 'VATICAN CITY', 'ESTONIA','LIECHTENSTEIN','LITHUANIA', 'LUXEMBOURG', 'MACEDONIA', 'MONTENEGRO'];
 
-var medImage =[];
-var hardImage =[];
-// var easyPic =
+
+
 
 //Global variables
 var wins = 0;                   // total number of wins (initially set to 0)
@@ -22,7 +19,6 @@ var word;                       // word from wordList that needs to be guessed b
 var index;
 var image;
 var wordList = easyList;        // wordList based on default or button pressed: either easy, medium, or hard
-var imageList = easyImage;
 var letter;                     // keeps track of the user letter pressed
 var incorrectKey;               // if incorrectKey = true then updates the guessedLetter array if the key pressed is an incorrect letter
 var proceed;                    // if proceed = true then the letter is not in the guessedLetter array or in the wordProgress array
@@ -33,7 +29,6 @@ var proceed;                    // if proceed = true then the letter is not in t
 //Chooses the easy, medium, or hard difficulty word lists based on button pressed
 function easyBtn(){
     wordList = easyList;
-    // imageList = easyImage;
     guessLeft = 12;
     modeGuessLeft = guessLeft;
     startGame();
@@ -42,7 +37,6 @@ function easyBtn(){
 
 function medBtn() {
     wordList = medList;
-    // imageList = medImage;
     guessLeft = 11;
     modeGuessLeft = guessLeft;
     startGame();
@@ -50,7 +44,6 @@ function medBtn() {
 
 function hardBtn() {
     wordList = hardList;
-    // imageList = hardImage;
     guessLeft = 10;
     modeGuessLeft = guessLeft;
     startGame();
@@ -62,7 +55,6 @@ function hardBtn() {
 function startGame() {
     index = Math.floor(Math.random() * wordList.length)
     word = wordList[index];
-    // image = imageList[index];
     guessedLetter =[];
     wordProgress = [];
 
@@ -127,11 +119,11 @@ function checkIfGuessCorrect(){
 
                 // if the user has completely guessed the word then user wins
                 if(wordProgress.indexOf('_') == -1){
-                    document.querySelector('.winOutcome').innerHTML = "<p>You Win!</p> <p>Click a level or press a letter to play again</p>";
+                    // document.querySelector('.winOutcome').innerHTML = "<p>You Win!</p> <p>Click a level or press a letter to play again</p>";
                     wins++;
                     document.querySelector('.wins').innerHTML = wins;
                     guessLeft = modeGuessLeft;
-                    // document.getElementById('globe').src= image;
+                    document.getElementById('globe').src= "assets/images/flags/" + word + ".png";
                     document.onkeyup = startGame;
                 }else{
                     document.onkeyup = keyPress;
@@ -152,6 +144,7 @@ function checkIfGuessCorrect(){
                 document.querySelector('.loseOutcome').innerHTML = "<p>You Lose!</p> <p>Click a level or press a letter to play again</p>";
                 losses++;
                 document.querySelector('.losses').innerHTML = losses;
+                document.getElementById('globe').src= "assets/images/antique.jpg";
                 guessLeft = modeGuessLeft;
                 document.onkeyup = startGame;
             }else{
